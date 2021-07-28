50ee81e8a1a1c67cd18d19dd7069da6462a8b36b
Ryan Peek <rapeek@ucdavis.edu>
2021-07-28 10:59:49 -0700

    update

--- gather_data.R
+++ gather_data.R
@@ -7,15 +7,17 @@ if(using_googlesheets){
   if(sheet_is_publicly_readable){
     # This tells google sheets to not try and authenticate. Note that this will only
     # work if your sheet has sharing set to "anyone with link can view"
-    sheets_deauth()
+    gs4_deauth()
   } else {
     # My info is in a public sheet so there's no need to do authentication but if you want
     # to use a private sheet, then this is the way you need to do it.
     # designate project-specific cache so we can render Rmd without problems
-    options(gargle_oauth_cache = ".secrets")
+    options(gargle_oauth_cache = here::here(".secrets"))
     
     # Need to run this once before knitting to cache an authentication token
-    # googlesheets4::sheets_auth()
+    # auth for sheets for readonly
+    #googlesheets4::gs4_auth(email = "rapeek@ucdavis.edu", cache = here::here(".secrets"),
+    #         scopes = "https://www.googleapis.com/auth/spreadsheets.readonly")
   }
   
   
